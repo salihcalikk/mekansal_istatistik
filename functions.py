6af48b36c6b58807c7dbeb057efda52723edcb25
salihcalikk <salihcalik95@gmail.com>
2022-12-03 14:12:39 +0300

    Added MannWhitneyUtest

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 import numpy as np
-from scipy.stats import chi2
+from scipy.stats import chi2, mannwhitneyu
 import re
+import pandas as pd
+from statistics import mean
 
 
 class ChiKare:
@@ -70,10 +72,89 @@ class ChiKare:
 
 
 class MannWhitneyUtest:
-    def __init__(self, a):
-        self.a = a
+    def __init__(self, array, q):
+        self.array = array
+        self.q = q
+
+    def sort_array(self, sort_field):
+        df = pd.DataFrame(self.array)
+        return df.sort_values(by=[sort_field], ascending=True)
+
+    @staticmethod
+    def get_repetitive_values(sorted_list):
+        repetitive_list = []
+        counter = 1
+        final_df = sorted_list
+        for index, row in final_df.iterrows():
+            if index != len(final_df) - 1 and row["count"] == final_df.iat[index + 1, 1]:
+                counter += 1
+                if counter == 2:
+                    repetitive_list.append({"count": row["count"], "index": [index, index + 1],
+                                       "new_values": [row["count"], row["count"] + 1]})
+                else:
+                    repetitive_list[len(repetitive_list) - 1]["index"].append(index + 1)
+                    repetitive_list[len(repetitive_list) - 1]["new_values"].append(repetitive_list[len(repetitive_list) - 1]["count"] + counter - 1)
+            else:
+                counter = 1
+            for key, i in enumerate(repetitive_list):
+                i["mean_value"] = mean(i["new_values"])
+        return [repetitive_list, final_df]
+
+    def ordered_new_values(self, sort_field):
+        sorted_list = self.sort_array(sort_field)
+        repetitive_list, ordered_list = self.get_repetitive_values(sorted_list)
+        for i in repetitive_list:
+            ordered_list.loc[i["index"][0]:i["index"][len(i["index"]) - 1], [sort_field]] = i["mean_value"]
+        return ordered_list
+
+    @staticmethod
+    def seperate_list(ordered_list, sort_field):
+        lists = []
+        gb = ordered_list.groupby(sort_field)
+        for x in gb.groups:
+            lists.append(gb.get_group(x))
+        return lists
+
+    @staticmethod
+    def u_test_score(sample_list, sum_column):
+        score_list = []
+        n_list = []
+        r_list = []
+        sum_column_index = sample_list[0].columns.get_loc(sum_column)
+        for i in sample_list:
+            row_len, column_len = i.shape
+            sum_column = i.sum(axis=0)[sum_column_index]
+            n_list.append(row_len)
+            r_list.append(sum_column)
+        for i in range(len(sample_list)):
+            score_list.append(np.prod(n_list) + ((n_list[i] * (n_list[i] + 1)) / 2) - r_list[i])
+        return min(score_list)
+
+    @staticmethod
+    def get_p_value(sample_list):
+        U1, p = mannwhitneyu(sample_list[0]["count"], sample_list[1]["count"])
+        return p
+
+    def result(self, count_column, sample_column):
+        ordered_list = self.ordered_new_values(count_column)
+        sample_list = self.seperate_list(ordered_list, sample_column)
+        u_value = self.u_test_score(sample_list, count_column)
+        p_value = self.get_p_value(sample_list)
+        result_text = ""
+        result = {"q": f"""%{self.q}""", "p_value": p_value,
+                "u_value": u_value}
+        if p_value < self.q:
+            result_text = f"""Yokluk Hipotezi Reddedilmiştir. Örneklemin %{self.q}
+             güven düzeyinde farklı olduğu sonucuna varılmıştır. İstatistiksel açıdan anlamlı bulunmuştur."""
+            result['description'] = re.sub(' +', ' ', result_text).replace('\n', '')
+        else:
+            result_text = f"""Yokluk Hipotezi Reddedilememiştir. Örneklemin %{self.q}
+             güven düzeyinde farklı olmadığı sonucuna varılmıştır."""
+            result['description'] = re.sub(' +', ' ', result_text).replace('\n', '')
+        return result
 
 
 class KruskalWallisHTest:
-    def __init__(self, a):
-        self.a = a
+    def __init__(self, array, q):
+        self.array = array
+        self.q = q
--- main.py
+++ main.py
@@ -1,11 +1,62 @@
-from functions import ChiKare
+from functions import ChiKare, MannWhitneyUtest, KruskalWallisHTest
 import numpy as np
 
 test_arrays = [{"method": "chi-kare", "q": 95, "array": np.array([[34, 63], [97, 16]])},
                {"method": "chi-kare", "q": 95, "array": np.array([[14, 43, 27, 45, 32], [16, 21, 34, 61, 15]])},
                {"method": "chi-kare", "q": 95, "array": np.array([[17, 2, 1, 1], [14, 3, 2, 1],
                                             [10, 1, 0, 0], [13, 2, 3, 2],
-                                            [19, 3, 2, 0]])}]
+                                            [19, 3, 2, 0]])},
+               {"method": "mann-whitney-u-test", "q": 95,
+                "array": [
+                    {"park_name": "Özgürlük Parkı", "count": 1, "borough": "A"},
+                    {"park_name": "Hastane Parkı", "count": 2, "borough": "B"},
+                    {"park_name": "Aslanlı Park", "count": 3, "borough": "A"},
+                    {"park_name": "Kral Mezarı Parkı", "count": 4, "borough": "B"},
+                    {"park_name": "Bayram Parkı", "count": 5, "borough": "A"},
+                    {"park_name": "Ayrancılar Anıtı", "count": 6, "borough": "A"},
+                    {"park_name": "Deniz Park", "count": 6, "borough": "A"},
+                    {"park_name": "Kuşluk Parkı", "count": 8, "borough": "B"},
+                    {"park_name": "Hatıra Ormanı", "count": 9, "borough": "A"},
+                    {"park_name": "Şahin Tepesi", "count": 10, "borough": "A"},
+                    {"park_name": "Gençlik Parkı", "count": 11, "borough": "B"},
+                    {"park_name": "Zafer Anıtı", "count": 12, "borough": "B"},
+                    {"park_name": "Sevgi Parkı", "count": 12, "borough": "A"},
+                    {"park_name": "İkinci Bahar Parkı", "count": 12, "borough": "B"},
+                    {"park_name": "Kurtuluş Parkı", "count": 15, "borough": "B"},
+                    {"park_name": "Sahil Parkı", "count": 16, "borough": "A"},
+                    {"park_name": "Büfe Park", "count": 17, "borough": "B"},
+                ]},
+               {"method": "kruskal-wallis-h-test", "q": 95,
+                "array": [
+                    {"construction_name": "Han Konağı", "count": 1, "borough": "A"},
+                    {"construction_name": "Ağalar Köşkü", "count": 2, "borough": "B"},
+                    {"construction_name": "Hanım Kökü", "count": 3, "borough": "D"},
+                    {"construction_name": "Aslanlı Kale", "count": 3, "borough": "A"},
+                    {"construction_name": "Beyaz Kule", "count": 3, "borough": "A"},
+                    {"construction_name": "Ağaçlı Çarşı", "count": 6, "borough": "A"},
+                    {"construction_name": "Aynalı Konak", "count": 7, "borough": "A"},
+                    {"construction_name": "Kapısız Kule", "count": 8, "borough": "B"},
+                    {"construction_name": "Sazlık Hanı", "count": 9, "borough": "A"},
+                    {"construction_name": "Saray Köşkü", "count": 10, "borough": "C"},
+                    {"construction_name": "Sıralı İkiz Konak", "count": 11, "borough": "A"},
+                    {"construction_name": "Züğürtler Köşkü", "count": 12, "borough": "B"},
+                    {"construction_name": "Sarmalı Konak", "count": 13, "borough": "B"},
+                    {"construction_name": "Yiğit Kulesi", "count": 14, "borough": "B"},
+                    {"construction_name": "Sırmalı Köşk", "count": 15, "borough": "D"},
+                    {"construction_name": "Mavi Saray", "count": 16, "borough": "C"},
+                    {"construction_name": "Arap Köşkü", "count": 17, "borough": "C"},
+                    {"construction_name": "Kapısız Köşk", "count": 18, "borough": "D"},
+                    {"construction_name": "Dere Han", "count": 19, "borough": "B"},
+                    {"construction_name": "Kuleli Çarşı", "count": 19, "borough": "A"},
+                    {"construction_name": "Laleli Köşk", "count": 21, "borough": "C"},
+                    {"construction_name": "Güllü Han", "count": 22, "borough": "C"},
+                    {"construction_name": "Sıvasız Kule", "count": 23, "borough": "D"},
+                    {"construction_name": "Sağır Han", "count": 24, "borough": "C"},
+                    {"construction_name": "Kuş Kulesi", "count": 24, "borough": "D"},
+                    {"construction_name": "Yazmalı Köşk", "count": 26, "borough": "D"},
+                    {"construction_name": "Edalı Konak", "count": 27, "borough": "C"},
+                ]}
+               ]
 
 result_list = []
 for i in test_arrays:
@@ -13,6 +64,13 @@ for i in test_arrays:
         chi_kare = ChiKare(array=i["array"], q=i["q"])
         result = chi_kare.chi_kare_result()
         result_list.append(result)
+    elif i["method"] == "mann-whitney-u-test":
+        mann_whitney = MannWhitneyUtest(array=i["array"], q=i["q"])
+        result = mann_whitney.result(count_column="count", sample_column="borough")
+        result_list.append(result)
+
+    elif i["method"] == "kruskal-wallis-h-test":
+        print("KruskalWallisHTest")
 
 for i in result_list:
     print(i)
